c9c2777fccfdd8c526b9da0ed28b76acecb349c9
Vlad Grytsenko <vladik.grytsenko2017@gmail.com>
2019-09-30 12:38:49 +0300

    Updates

---
+++ precache-manifest.2516745502ddf637617de885b1060935.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d1f62c9b7fba8794acef1b02e525f96",
+    "url": "/react_tabs-with-router/index.html"
+  },
+  {
+    "revision": "a2a4ec3cef3f5df7a713",
+    "url": "/react_tabs-with-router/static/css/main.cfebd89a.chunk.css"
+  },
+  {
+    "revision": "3f885f21c29c34f0e56c",
+    "url": "/react_tabs-with-router/static/js/2.dcfce78c.chunk.js"
+  },
+  {
+    "revision": "a2a4ec3cef3f5df7a713",
+    "url": "/react_tabs-with-router/static/js/main.c8e2ff82.chunk.js"
+  },
+  {
+    "revision": "da3ca6fceb301548d22d",
+    "url": "/react_tabs-with-router/static/js/runtime~main.5858125e.js"
+  }
+]);
